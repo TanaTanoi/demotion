86dde48dde2243a4171eabc970ca7774491890ba
Ioritana Tanoi <tanoitana@c-05-06-lab.mddn.vuw.ac.nz>
2017-03-24 11:10:57 +1300

    Add cooldown to PlayerMovement script
    Prevents spamming of the boost

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -12,10 +12,14 @@ public class PlayerMovement : MonoBehaviour {
 
 	public float BOOST_POWER = 250;
 	public float ROTATION_SPEED = 0.09f;
+	public float DEFAULT_COOLDOWN = 0.7f;
+
+	public float boostCooldown;
+
+	private float timestamp = 0.0f;
 	// Use this for initialization
 	void Start () {
-
-		
+		boostCooldown = DEFAULT_COOLDOWN;
 	}
 	
 	// Update is called once per frame
@@ -26,10 +30,11 @@ public class PlayerMovement : MonoBehaviour {
 
 		moveTowardsDesiredDirection ();
 
-		bool boosting = Input.GetButtonDown ("Fire1");
+		bool boosting = Input.GetAxisRaw ("Fire1") != 0f;
 
-		if (boosting) {
+		if (boosting && timestamp <= Time.time) {
 			MovementSphere.AddForce (aimDirection * BOOST_POWER);
+			timestamp = Time.time + boostCooldown;
 		}
 	}
 
@@ -37,9 +42,6 @@ public class PlayerMovement : MonoBehaviour {
 
 	void moveTowardsDesiredDirection(){
 		aimDirection = Vector3.RotateTowards (aimDirection, desiredDirection, ROTATION_SPEED, 0f);
-//		float newY = Mathf.Atan2 (aimDirection.x, aimDirection.z);
-
-//		DirectionObject.rotation = Quaternion.Euler (new Vector3 (DirectionObject.rotation.x, newY, DirectionObject.rotation.z));
 		DirectionObject.rotation = Quaternion.LookRotation (aimDirection, new Vector3 (0, 1, 0));
 		DirectionObject.position = MovementSphere.transform.position;
 	}
