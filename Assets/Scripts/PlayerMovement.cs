0d66bd4c6bb2ac7415454debda186b3f39b90adb
DarrenHobern <darren.hobern@gmail.com>
2017-03-24 17:13:03 +1300

    Began creating chair placeholder, created template for platform specific controls, tweaked playerMovement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float DEFAULT_BOOST_POWER = 250;
+	public float DEFAULT_ROTATION_SPEED = 0.09f;
+	public float DEFAULT_COOLDOWN = 0.7f;
+
+	private float boostCooldown;
+    private float boostPower;
+    private float rotationSpeed;
+
+	private float timestamp = 0.0f;
+    private Vector3 aimDirection = Vector3.forward;
+    private Vector3 desiredDirection;
+
+    private Rigidbody chairRigidbody;
+
+    // Use this for initialization
+    void Start () {
+		boostCooldown = DEFAULT_COOLDOWN;
+        boostPower = DEFAULT_BOOST_POWER;
+        rotationSpeed = DEFAULT_ROTATION_SPEED;
+
+        chairRigidbody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontalInput = Input.GetAxis ("Horizontal");
+		float verticalInput = Input.GetAxis ("Vertical");
+
+		desiredDirection = Vector3.Normalize(new Vector3(horizontalInput, 0f , verticalInput));
+
+		moveTowardsDesiredDirection ();
+
+		bool boosting = Input.GetAxisRaw ("Boost") != 0f;
+
+		if (boosting) {
+            if (timestamp <= Time.time) {
+                chairRigidbody.AddRelativeForce(new Vector3(0.0f, 0.0f, boostPower));
+                timestamp = Time.time + boostCooldown;
+            }
+		}
+	}
+
+
+    public float getBoostCooldown()
+    {
+        return boostCooldown;
+    }
+
+	void moveTowardsDesiredDirection(){
+		aimDirection = Vector3.RotateTowards (aimDirection, desiredDirection, rotationSpeed, 0f);
+		transform.rotation = Quaternion.LookRotation (aimDirection, new Vector3 (0, 1, 0));
+		//DirectionObject.position = transform.position;
+	}
+}
---
+++ Assets/Scripts/ScriptGameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptGameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        Debug.Log(Application.platform);
+        switch(Application.platform)
+        {
+            case RuntimePlatform.WindowsPlayer:
+                //Change to windows controls
+                break;
+            case RuntimePlatform.LinuxPlayer:
+                //Change to linux controls
+                break;
+            case RuntimePlatform.OSXPlayer:
+                //Change to OSX controlss
+                break;
+        }
+        //Set default input controls based on operating system game is runnning on.
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Control game timer / scores, etc
+	}
+}
