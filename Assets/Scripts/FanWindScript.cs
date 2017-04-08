b0a5747f9b67906e4fd8ff49e4c60bacc087a229
DarrenHobern <darren.hobern@gmail.com>
2017-04-08 23:58:02 +1200

    Prototyping fan script

---
+++ Assets/Scripts/FanWindScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanWindScript : MonoBehaviour {
+
+    public float windForce = 3f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        Rigidbody otherRB = other.GetComponentInParent<Rigidbody>();
+        otherRB.AddForceAtPosition(new Vector3(0,0,-windForce), otherRB.centerOfMass);
+        Debug.Log("Windy here!");
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public const float DEFAULT_BOOST_POWER = 250;
-	public const float DEFAULT_ROTATION_SPEED = 0.09f;
-	public const float DEFAULT_COOLDOWN = 0.7f;
+	public static float DEFAULT_BOOST_POWER = 250;
+	public static float DEFAULT_ROTATION_SPEED = 0.09f;
+	public static float DEFAULT_COOLDOWN = 0.7f;
 
 	private float boostCooldown;
     private float boostPower;
@@ -36,7 +36,7 @@ public class PlayerMovement : MonoBehaviour {
 
 		moveTowardsDesiredDirection ();
 
-		if (Input.GetAxisRaw("Boost") != 0) {
+		if (Input.GetAxisRaw("Boost") > 0) {
             if (timestamp <= Time.time) {
                 chairRigidbody.AddRelativeForce(new Vector3(0.0f, 0.0f, boostPower));
                 timestamp = Time.time + boostCooldown;
@@ -65,6 +65,5 @@ public class PlayerMovement : MonoBehaviour {
 	void moveTowardsDesiredDirection(){
 		aimDirection = Vector3.RotateTowards (aimDirection, desiredDirection, rotationSpeed, 0f);
 		transform.rotation = Quaternion.LookRotation (aimDirection, new Vector3 (0, 1, 0));
-		//DirectionObject.position = transform.position;
 	}
 }
