e1d212cab445fe16331bbfa1585fca9ee51d48c8
DarrenHobern <darren.hobern@gmail.com>
2017-04-22 16:16:10 +1200

    Completed setup for navigation bar of options menu.

--- Assets/Scripts/OptionsMenuManager.cs
+++ Assets/Scripts/OptionsMenuManager.cs
@@ -5,15 +5,70 @@ using UnityEngine;
 
 public class OptionsMenuManager : MonoBehaviour {
 
-    public GameObject[] optionSubMenus;
-    
-	// Use this for initialization
+    public GameObject VisualMenu;
+    public GameObject AudioMenu;
+    public GameObject ControlsMenu;
+    public GameObject DataMenu;
+
+    private GameObject selectedMenu = null;
+
+
 	void Start () {
-		
+        if(VisualMenu == null)
+        {
+            Debug.LogError("Visual menu must be assigned");
+        } else
+        {
+            // Default menu is the visuals menu
+            selectedMenu = VisualMenu;
+        }
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    /**
+     * Switches to the visual menu
+     */
+    public void SwitchToVisualMenu()
+    {
+        SwitchMenu(VisualMenu);
+    }
+
+    /**
+     * Switches to the audio menu
+     */
+    public void SwitchToAudioMenu()
+    {
+        SwitchMenu(AudioMenu);
+    }
+
+    /**
+     * Switches to the controls menu
+     */
+    public void SwitchToControlsMenu()
+    {
+        SwitchMenu(ControlsMenu);
+    }
+
+    /**
+     * Switches to the data menu
+     */
+    public void SwitchToDataMenu()
+    {
+        SwitchMenu(DataMenu);
+    }
+
+    /**
+     * If there is a change in menu,
+     * deactivates the old menu
+     * and activates the new one.
+     */
+    private void SwitchMenu(GameObject menu)
+    {
+        if(selectedMenu != menu)
+        {
+            selectedMenu.SetActive(false);
+            selectedMenu = menu;
+            selectedMenu.SetActive(true);
+        }
+    }
 }
