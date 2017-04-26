96fe7872dff4dab6cba756174414632daaa0d1df
DarrenHobern <darren.hobern@gmail.com>
2017-04-26 15:34:38 +1200

    Added in game timer to HUD. played with textures, no real changes.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public Text timerText;
+    public float timeLeft = 99;
+
+	// Use this for initialization
+	void Start () {
+        timerText.text = "Time: " + (int)timeLeft;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        timeLeft -= Time.deltaTime;
+        timerText.text = "Time: " + (int)timeLeft;
+    }
+}
---
+++ Assets/Scripts/PlayerHitDetection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHitDetection : MonoBehaviour {
+
+    public float hitThreshold;
+
+    private GameController gameControl;
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // If we have been hit with enough force, play a sound. 
+        if(collision.relativeVelocity.magnitude > hitThreshold)
+        {
+            //Play hit sound here
+            
+            // If we have been hit by a hostile lance then lose a life or something
+            if(collision.gameObject.CompareTag("Lance"))
+            {
+
+            }
+        }
+    }
+}
