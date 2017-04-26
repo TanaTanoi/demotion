8396556cb78b0384ba96dc0e2c3adf4f818da79e
DarrenHobern <darren.hobern@gmail.com>
2017-04-27 09:10:53 +1200

    Working on lance hit detection

--- Assets/Scripts/PlayerHitDetection.cs
+++ Assets/Scripts/PlayerHitDetection.cs
@@ -13,17 +13,23 @@ public class PlayerHitDetection : MonoBehaviour {
         
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("HIt deteceteded");
         // If we have been hit with enough force, play a sound. 
         if(collision.relativeVelocity.magnitude > hitThreshold)
         {
             //Play hit sound here
-            
+            Debug.Log("Sound here");
             // If we have been hit by a hostile lance then lose a life or something
             if(collision.gameObject.CompareTag("Lance"))
             {
-
+                Debug.Log("I've been attacked!");
             }
         }
     }
