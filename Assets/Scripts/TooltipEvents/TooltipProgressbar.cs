6004ab021fe6c4a5b8052b881dd3d850b81a8f6a
TanaTanoi <tana.tanoi@gmail.com>
2017-05-12 11:25:35 +1200

    Removed models and unwanted stuff

---
+++ Assets/Scripts/TooltipEvents/TooltipProgressbar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TooltipProgressbar : MonoBehaviour {
+
+    public RectTransform panel;
+    private Vector3 originalScale;
+    private Vector3 originalPos;
+
+    private float max = 4.5f;
+	void Start () {
+        originalPos = panel.localPosition;
+        originalScale = panel.localScale;
+	}
+
+    public void SetProgress(float percent) {
+
+        percent = percent * 4.5f;
+
+        panel.localScale =  (Vector3.right * 0.1f * percent) + (originalScale.x * Vector3.up);
+        panel.localPosition = originalPos +  (Vector3.right * percent * 50);
+    }
+}
