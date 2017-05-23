a8a6f75180be9b0f13e198460083a513e9130c3c
ashtondavvo <ashton.d173@live.com>
2017-05-24 10:18:58 +1200

    pushing changes to my scene and ragdoll prefab

--- Assets/Scripts/LanceHit.cs
+++ Assets/Scripts/LanceHit.cs
@@ -21,6 +21,30 @@ public class LanceHit : MonoBehaviour {
 			Destroy (other.gameObject);
 			GameObject ragDoll = (GameObject)Instantiate(Resources.Load("Ragdoll - final"), other.transform.position, other.transform.rotation);
 			ragDoll.GetComponent<Animator> ().enabled = false;
+			// GameController.onHit(this.getPLayer(), getOther(other))
+			Debug.Log(getPLayer());
+			getOther (other);
 		}
 	}
+
+	int getPLayer(){
+		Debug.Log (this.gameObject.ToString());
+		if (this.gameObject.ToString().Equals("Player1 Lance"))
+			return 1;
+		else if (this.gameObject.ToString().Equals("Player2 Lance"))
+			return 2;
+		else if (this.gameObject.ToString().Equals("Player3 Lance"))
+			return 3;
+		else if (this.gameObject.ToString().Equals("Player4 Lance"))
+			return 4;
+		
+		return 0;
+	}
+
+	int getOther(Collision other){
+		Debug.Log (other.gameObject);
+		return 0;
+	}
+
+
 }
