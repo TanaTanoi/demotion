d5104a62d233c99409b6339b5b7a4274aadbb128
ashtondavvo <ashton.d173@live.com>
2017-05-24 12:19:04 +1200

    changed player prefab

--- Assets/Scripts/LanceHit.cs
+++ Assets/Scripts/LanceHit.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 
 public class LanceHit : MonoBehaviour {
 
+	FixedJoint playerHolding;
 
 	void Start(){
-
+		playerHolding = this.GetComponent<FixedJoint> ();
 	}
 
 	void Update(){
-
+		if (playerHolding.connectedBody == null) {
+			Destroy (this);
+		}
 	}
 
 	void OnCollisionEnter(Collision other)
@@ -21,28 +24,43 @@ public class LanceHit : MonoBehaviour {
 			Destroy (other.gameObject);
 			GameObject ragDoll = (GameObject)Instantiate(Resources.Load("Ragdoll - final"), other.transform.position, other.transform.rotation);
 			ragDoll.GetComponent<Animator> ().enabled = false;
+			GameObject chair = (GameObject) other.transform.Find ("Chair").gameObject;
+			chair.transform.parent = null;
+			chair.AddComponent<Rigidbody>();
 			// GameController.onHit(this.getPLayer(), getOther(other))
 			Debug.Log(getPLayer());
-			getOther (other);
+			Debug.Log(getOther (other));
 		}
 	}
 
+	/**
+	 * Gets the player number for the player holding the lance
+	 **/
 	int getPLayer(){
-		Debug.Log (this.gameObject.ToString());
-		if (this.gameObject.ToString().Equals("Player1 Lance"))
+		if (this.gameObject.name.Equals("Player1 Lance"))
 			return 1;
-		else if (this.gameObject.ToString().Equals("Player2 Lance"))
+		else if (this.gameObject.name.Equals("Player2 Lance"))
 			return 2;
-		else if (this.gameObject.ToString().Equals("Player3 Lance"))
+		else if (this.gameObject.name.Equals("Player3 Lance"))
 			return 3;
-		else if (this.gameObject.ToString().Equals("Player4 Lance"))
+		else if (this.gameObject.name.Equals("Player4 Lance"))
 			return 4;
 		
 		return 0;
 	}
 
+	/**
+	 * Gets the player number for the player who got hit by the lance
+	 **/
 	int getOther(Collision other){
-		Debug.Log (other.gameObject);
+		if (other.gameObject.name.Equals("Player1"))
+			return 1;
+		else if (other.gameObject.name.Equals("Player2"))
+			return 2;
+		else if (other.gameObject.name.Equals("Player3"))
+			return 3;
+		else if (other.gameObject.name.Equals("Player4"))
+			return 4;
 		return 0;
 	}
 
