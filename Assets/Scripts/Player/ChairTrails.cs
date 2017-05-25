3928b4bba68cf0e116e8b46e840b6e94fe7e7a2a
AndrewMoffat <moffatandr@myvuw.ac.nz>
2017-05-25 13:50:54 +1200

    Added wall prefabs and fixed desk material

---
+++ Assets/Scripts/Player/ChairTrails.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairTrails : MonoBehaviour {
+    List<Vector3> trailPoints = new List<Vector3>();
+    public GameObject prefab;
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        trailPoints.Add(transform.position);
+        for(int i=0;i<trailPoints.Count-1;i++)
+        {
+            Debug.DrawLine(trailPoints[i], trailPoints[i + 1], Color.red);
+           // Instantiate(prefab, trailpos, Quaternion.identity);
+        }
+	}
+}
