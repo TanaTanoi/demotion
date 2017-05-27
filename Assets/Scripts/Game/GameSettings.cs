5d16ffeed835389a7c3e3ce84ebdb3527571def4
DarrenHobern <darren.hobern@gmail.com>
2017-05-28 09:37:08 +1200

    added defaults to gamesettings

--- Assets/Scripts/Game/GameSettings.cs
+++ Assets/Scripts/Game/GameSettings.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Data", menuName = "Game/Settings", order = 1)]
 public class GameSettings : ScriptableObject {
     public Dictionary<int, InputType> IDtoInput;
-    public int playerCount;
-    public GameSetup.GameMode mode;
-    public int numberRounds;
-    public float roundDuration;
-    public float respawnTime;
-    public int maxLives;
-    public int targetScore;
-    public int targetKills;
+    public int playerCount = 2;
+    public GameSetup.GameMode mode = GameSetup.GameMode.DEMOTION;
+    public int numberRounds = 3;
+    public float roundDuration = 60.0f;
+    public float respawnTime = 3f;
+    public int maxLives = 5;
+    public int targetScore = 5000;
+    public int targetKills = 20;
     
 }
