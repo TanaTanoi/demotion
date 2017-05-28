2bd651225336e62979603b1b7a8f82f7eb515ceb
Tayler Barkle-Barnett <taylerbarkle-barnett@Taylers-MacBook-Pro.local>
2017-05-28 12:38:47 +1200

    edit ArenaGenerator to generate wood tiles as well as carpet tiles. edit floor tile prefabs

--- Assets/Scripts/Game/ArenaGenerator.cs
+++ Assets/Scripts/Game/ArenaGenerator.cs
@@ -7,24 +7,34 @@ public class ArenaGenerator : MonoBehaviour {
 
 	public GameObject floor;
 	public GameObject[] wallPrefabs;
-	public GameObject[] tilePrefabs;
-	public GameObject[] tileClutteredPrefabs;
-	public GameObject[] tileCrackedPrefabs;
+	public GameObject[] tileCarpetPrefabs;
+	public GameObject[] tileCarpetClutteredPrefabs;
+	public GameObject[] tileCarpetCrackedPrefabs;
 
-	List<Vector3> tileLocations = new List<Vector3>();
+	public GameObject[] tileWoodPrefabs;
+	public GameObject[] tileWoodClutteredPrefabs;
+	public GameObject[] tileWoodCrackedPrefabs;
+
+	List<Vector3> tileCarpetLocations = new List<Vector3>();
+	List<Vector3> tileWoodLocations = new List<Vector3>();
+
 	GameObject arenaOutline;
 
 	GameObject SpawnPoints;
 
 	// Use this for initialization
-	void Start () {
+	void Start(){
+		Generate ();
+	}
+
+	void Generate () {
 		SpawnPoints = GameObject.Find ("SpawnPoints");
 		PopulateLists ();
-
-		Debug.Log (wallPrefabs.Length);
-		Debug.Log (tilePrefabs.Length);
-		Debug.Log (tileClutteredPrefabs.Length);
-		Debug.Log (tileCrackedPrefabs.Length);
+//
+//		Debug.Log (wallPrefabs.Length);
+//		Debug.Log (tilePrefabs.Length);
+//		Debug.Log (tileClutteredPrefabs.Length);
+//		Debug.Log (tileCrackedPrefabs.Length);
 
 		GenerateArena ();
 		GenerateStartPoints ();
@@ -33,9 +43,13 @@ public class ArenaGenerator : MonoBehaviour {
 	void PopulateLists()
 	{
 		wallPrefabs = Resources.LoadAll<GameObject>("Walls");
-		tilePrefabs = Resources.LoadAll<GameObject>("FloorTiles/TilesTest");
-		tileClutteredPrefabs = Resources.LoadAll<GameObject>("FloorTiles/ClutteredTilesTest");
-		tileCrackedPrefabs = Resources.LoadAll<GameObject>("FloorTiles/CrackedTilesTest");
+		tileCarpetPrefabs = Resources.LoadAll<GameObject>("FloorTiles/CarpetClear");
+		tileCarpetClutteredPrefabs = Resources.LoadAll<GameObject>("FloorTiles/CarpetCluttered");
+		tileCarpetCrackedPrefabs = Resources.LoadAll<GameObject>("FloorTiles/CarpetCracked");
+	
+		tileWoodPrefabs = Resources.LoadAll<GameObject>("FloorTiles/WoodClear");
+		tileWoodClutteredPrefabs = Resources.LoadAll<GameObject>("FloorTiles/WoodCluttered");
+		tileWoodCrackedPrefabs = Resources.LoadAll<GameObject>("FloorTiles/WoodCracked");
 	}
 
 	void GenerateStartPoints(){
@@ -49,21 +63,19 @@ public class ArenaGenerator : MonoBehaviour {
 				spawnLocations.Add (child.position);
 			}
 		}
-			
-		Debug.Log (spawnLocations.Count);
 
 		foreach (Vector3 spawnLocation in spawnLocations) {
 			GameObject spawn = Instantiate (spawnPoint) as GameObject;
 			spawn.transform.position = spawnLocation;
 			spawn.transform.position += new Vector3(0,2,0);
 			spawn.transform.parent = SpawnPoints.transform;
-	
 		}
 	}
 
 	public void GenerateArena(){
 		GenerateOutline ();
-		GenerateTiles ();
+		GenerateCarpetTiles ();
+		GenerateWoodTiles ();
 	}
 
 	void GenerateOutline(){
@@ -75,32 +87,36 @@ public class ArenaGenerator : MonoBehaviour {
 //		}
 
 		int randomIndex = Random.Range (0, wallPrefabs.Length);
-		GameObject prefabObject = Instantiate(wallPrefabs[randomIndex]) as GameObject; 
+		GameObject prefabObject = Instantiate(wallPrefabs[randomIndex], Vector3.zero, Quaternion.identity) as GameObject; 
 		walls.Add (prefabObject);
 
 		int randomIndex2 = Random.Range (0, walls.Count);
 		arenaOutline = walls [randomIndex2];
 	
-		string tag = "tileLocation";
-		foreach (Transform child in arenaOutline.transform) {
-			if (child.tag == tag) {
-				tileLocations.Add (child.position);
-			}
+		string carpetLocationTag = "tileLocation";
+		string woodLocationTag = "woodTileLocation";
 
+		foreach (Transform child in arenaOutline.transform) {
+			if (child.tag == carpetLocationTag) {
+				tileCarpetLocations.Add (child.position);
+			} else if (child.tag == woodLocationTag) {
+				tileWoodLocations.Add (child.position);
+			}
 		}
-			
 	}
 
-	void GenerateTiles(){
+	// generate all the carpet tile objects
+	void GenerateCarpetTiles(){
 		List<GameObject> tiles = new List<GameObject>();
 
-		int count = tileLocations.Count;
-		int tileCount = (tileLocations.Count)/2;
-		count -= tileCount;
+		int count = tileCarpetLocations.Count;
+		int tileClearCount = (tileCarpetLocations.Count)/2;
+		count -= tileClearCount;
 
-		for (int i = 0; i < tileCount; i++) {
-			int randomIndex = Random.Range (0, tilePrefabs.Length);
-			GameObject prefabObject = Instantiate(tilePrefabs[randomIndex]) as GameObject; 
+		// adding "clear" carpet objects
+		for (int i = 0; i < tileClearCount; i++) {
+			int randomIndex = Random.Range (0, tileCarpetPrefabs.Length);
+			GameObject prefabObject = Instantiate(tileCarpetPrefabs[randomIndex]) as GameObject; 
 			tiles.Add (prefabObject);
 		}
 			
@@ -108,42 +124,98 @@ public class ArenaGenerator : MonoBehaviour {
 
 		count -= crackedTileCount;
 
+		// adding "cluttered" carpet objects
 		for (int i = 0; i < count; i++) {
-			int randomIndex = Random.Range (0, tileClutteredPrefabs.Length);
-			GameObject prefabObject = Instantiate(tileClutteredPrefabs[randomIndex]) as GameObject; 
+			int randomIndex = Random.Range (0, tileCarpetClutteredPrefabs.Length);
+			GameObject prefabObject = Instantiate(tileCarpetClutteredPrefabs[randomIndex]) as GameObject; 
 			tiles.Add (prefabObject);
 		}
 
+		// adding "cracked" carpet objects
 		for (int i = 0; i < crackedTileCount; i++) {
-			int randomIndex = Random.Range (0, tileCrackedPrefabs.Length);
-			GameObject prefabObject = Instantiate(tileCrackedPrefabs[randomIndex]) as GameObject; 
+			int randomIndex = Random.Range (0, tileCarpetCrackedPrefabs.Length);
+			GameObject prefabObject = Instantiate(tileCarpetCrackedPrefabs[randomIndex]) as GameObject; 
+			tiles.Add (prefabObject);
+		}
+			
+		int tilesNum = tiles.Count;
+
+		for (int i = 0; i < tilesNum; i++) {
+			int randomIndex = Random.Range (0, tiles.Count);
+			GameObject tile = tiles [randomIndex];
+			PlaceTile (tile, true);
+			//tile.transform.parent = floor.transform;
+			tiles.RemoveAt (randomIndex);
+		}
+	}
+
+	// generate all the wood tile objects
+	void GenerateWoodTiles(){
+		List<GameObject> tiles = new List<GameObject>();
+
+		int count = tileWoodLocations.Count;
+		int tileClearCount = (tileWoodLocations.Count)/2;
+		count -= tileClearCount;
+
+		// adding "clear" wood objects
+		for (int i = 0; i < tileClearCount; i++) {
+			int randomIndex = Random.Range (0, tileWoodPrefabs.Length);
+			GameObject prefabObject = Instantiate(tileWoodPrefabs[randomIndex]) as GameObject; 
 			tiles.Add (prefabObject);
 		}
 
+		int crackedTileCount = count / 4;
+
+		count -= crackedTileCount;
+
+		// adding "cluttered" wood objects
+		for (int i = 0; i < count; i++) {
+			int randomIndex = Random.Range (0, tileWoodClutteredPrefabs.Length);
+			GameObject prefabObject = Instantiate(tileWoodClutteredPrefabs[randomIndex]) as GameObject; 
+			tiles.Add (prefabObject);
+		}
+
+		// adding "cracked" wood objects
+		for (int i = 0; i < crackedTileCount; i++) {
+			int randomIndex = Random.Range (0, tileWoodCrackedPrefabs.Length);
+			GameObject prefabObject = Instantiate(tileWoodCrackedPrefabs[randomIndex]) as GameObject; 
+			tiles.Add (prefabObject);
+		}
 
 		int tilesNum = tiles.Count;
 
 		for (int i = 0; i < tilesNum; i++) {
 			int randomIndex = Random.Range (0, tiles.Count);
 			GameObject tile = tiles [randomIndex];
-			PlaceTile (tile);
+			PlaceTile (tile, false);
 			//tile.transform.parent = floor.transform;
 			tiles.RemoveAt (randomIndex);
 		}
 	}
+		
 
-	void PlaceTile(GameObject tile){
+	void PlaceTile(GameObject tile, bool isCarpet){
+		bool carpetTile = isCarpet;
+
 		List<int> possibleRotations = new List<int>(){0, 90, 180, 270};
 		int rotateBy = possibleRotations[Random.Range(0,possibleRotations.Count)];
 		tile.transform.Rotate (0, rotateBy, 0);
 
-		tile.transform.position = TilePointReference ();
+		tile.transform.position = TilePointReference (carpetTile);
 	}
 
-	Vector3 TilePointReference(){
-		int randomIndex = Random.Range (0, tileLocations.Count);
-		Vector3 toReturn = tileLocations [randomIndex];
-		tileLocations.RemoveAt (randomIndex);
+	Vector3 TilePointReference(bool isCarpet){
+		Vector3 toReturn;
+		if (isCarpet) {
+			int randomIndex = Random.Range (0, tileCarpetLocations.Count);
+			toReturn = tileCarpetLocations [randomIndex];
+			tileCarpetLocations.RemoveAt (randomIndex);
+		} else {
+			int randomIndex = Random.Range (0, tileWoodLocations.Count);
+			toReturn = tileWoodLocations [randomIndex];
+			tileWoodLocations.RemoveAt (randomIndex);
+		}
+
 		return toReturn;
 	}
 
