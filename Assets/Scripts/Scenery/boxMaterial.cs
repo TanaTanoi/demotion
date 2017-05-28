07152d6f0161df0fdbe0a28f25bcc8d8d33d4f20
Guest User <Guest@c-05-06-lab.mddn.vuw.ac.nz>
2017-05-28 12:29:21 +1200

    Added paper particles

---
+++ Assets/Scripts/Scenery/boxMaterial.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class boxMaterial : MonoBehaviour
+{
+    private ParticleSystem psystem;
+	private int paperCount=3;
+    void Start()
+    {
+		psystem = GetComponentInChildren<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    void OnCollisionEnter(Collision col)
+    {
+		if (col.gameObject.tag == "Player"&&paperCount>0) { 
+            psystem.Play();
+			paperCount--;
+         }
+    }
+}
+    
--- Assets/Scripts/Scenery/storageMaterial.cs
+++ Assets/Scripts/Scenery/storageMaterial.cs
@@ -5,43 +5,42 @@ using UnityEditor;
 
 public class storageMaterial : MonoBehaviour
 {
-    private ParticleSystem psystem;
-    private bool coltrack = false;
-    // Use this for initialization
-    void Start()
-    {
-        psystem = GetComponentInChildren<ParticleSystem>();
-        int rand = Random.Range(0, 3);
-        string Customization = "STORAGERED";
-        if (rand == 1)
-        {
-            Customization = "STORAGEBLUE";
-        }
-        else if (rand == 2)
-        {
-            Customization = "STORAGEGREEN";
-        }
-        else if (rand == 3)
-        {
-            Customization = "STORAGERED";
-        }
-        string matPath = "Assets/Materials&Textures/Storage/" + Customization + ".mat";
-        Material newMat = (Material)AssetDatabase.LoadAssetAtPath(matPath, typeof(Material));
-        MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
-        meshRenderer.material = newMat;
-    }
+	private ParticleSystem psystem;
+	private bool coltrack = false;
+	// Use this for initialization
+	void Start()
+	{
+		psystem = GetComponentInChildren<ParticleSystem>();
+		int rand = Random.Range(0, 3);
+		string Customization = "STORAGERED";
+		if (rand == 1)
+		{
+			Customization = "STORAGEBLUE";
+		}
+		else if (rand == 2)
+		{
+			Customization = "STORAGEGREEN";
+		}
+		else if (rand == 3)
+		{
+			Customization = "STORAGERED";
+		}
+		string matPath = "Assets/Materials&Textures/Storage/" + Customization + ".mat";
+		Material newMat = (Material)AssetDatabase.LoadAssetAtPath(matPath, typeof(Material));
+		MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
+		meshRenderer.material = newMat;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
 
 
-    }
-    void OnCollisionEnter(Collision col)
-    {
-        if (col.gameObject.tag == "Player") { 
-            psystem.Play();
-         }
-    }
+	}
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "Player") { 
+			psystem.Play();
+		}
+	}
 }
-    
