344380a2686b74f212bf71ce7e6d11033e3dddee
TanaTanoi <tana.tanoi@gmail.com>
2017-06-02 12:47:04 +1200

    Add targeting throwables
    Much much easier to hit other players now

--- Assets/Scripts/Items/ThrowableItem.cs
+++ Assets/Scripts/Items/ThrowableItem.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class ThrowableItem : MonoBehaviour {
 
-	public enum Effect { SPIN, STICK, Count }
-
-	public Effect effect;
-
+	private Transform target;
+	private Rigidbody rb; 
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody> ();
+		target = SelectTargetPlayer ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		rb.AddForce ((target.position - transform.position).normalized * (rb.velocity.normalized.magnitude * 25));
 	}
 
-	void HitPlayer(PlayerMovement player){
-
+	public Transform SelectTargetPlayer(){
+		PlayerMovement[] players = FindObjectsOfType<PlayerMovement> ();
+		Vector3 dir = rb.velocity.normalized;
+		float bestDir = -1;
+		Transform bestPlayer = players[0].transform;
+		foreach (PlayerMovement player in players) {
+			Vector3 oDir = (player.transform.position - transform.position).normalized;
+			float d = Vector3.Dot (oDir, dir);
+			if (bestDir < d) {
+				bestDir = d;
+				bestPlayer = player.transform;
+			}
+		}
+		return bestPlayer;
 	}
+
 }
