a5a70fefa758cc42ca8e75bd7ee9db63816848cc
TBBarnett <tayler.barklebarnett@gmail.com>
2017-06-09 13:51:59 +1200

    reverted playerSettings file

--- Assets/Scripts/Player/PlayerSettings.cs
+++ Assets/Scripts/Player/PlayerSettings.cs
@@ -5,41 +5,29 @@ using UnityEngine;
 /**
  * Settings for this player
  */
-public class PlayerSettings : MonoBehaviour {
+public class PlayerSettings {
 
     public InputType input;
     public int controllerID;
+	public int keyboardID;
     public int playerID;
     public int teamID;
+    //private PlayerPref customisation; // customisation settings
 	public SkinIndexs indices;
 
-
-	public PlayerSettings(InputType input, int pid, int tid, SkinIndexs indices)
+    public PlayerSettings(InputType input, int pid, int inputid, int tid)
     {
-        if(input == InputType.Controller)
-        {
-            throw new System.ArgumentException("Error, using the wrong constructor with InputType; controller.");
-        }
+		if (input == InputType.Controller) {
+			this.controllerID = inputid;
+			this.keyboardID = -1;
+		} else if (input == InputType.Keyboard) {
+			this.keyboardID = inputid;
+			this.controllerID = -1;
+		}
 
         this.input = input;
-        this.controllerID = -1;
         this.playerID = pid;
         this.teamID = tid;
+		//this.indices = indices;
     }
-
-	public PlayerSettings(InputType input, int cid, int pid, int tid, SkinIndexs indices)
-    {
-        if (input != InputType.Controller)
-        {
-            Debug.Log(input);
-            throw new System.ArgumentException("Error, using the controller constructor when InputType is not controller.");
-        }
-
-        this.input = input;
-        this.controllerID = cid;
-        this.playerID = pid;
-        this.teamID = tid;
-    }
-
-
 }
