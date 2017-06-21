9d9bfb1b04c618199d88f99abf486c19244c8c99
TBBarnett <tayler.barklebarnett@gmail.com>
2017-06-21 13:30:25 +1200

    edit main menu, add new font and image

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -7,14 +7,13 @@ using UnityEngine;
 public class MenuController : MonoBehaviour {
 
     public GameObject DefaultPanel;
-
     private GameObject selectedPanel;
     private bool isPaused;
 
 	// Use this for initialization
 	void Start () {
         selectedPanel = DefaultPanel;
-        SwitchMenu(selectedPanel);
+		SwitchMenu (selectedPanel);
     }
 
     private void OnApplicationFocus(bool focus)
@@ -59,10 +58,19 @@ public class MenuController : MonoBehaviour {
             selectedPanel = menu;
             selectedPanel.SetActive(true);
         }
+		Debug.Log ("clicked");
     }
 
 	public void SwitchScene(string scene)
     {
 		SceneManager.LoadSceneAsync(scene, LoadSceneMode.Single);
     }
+
+	public void ShowQuitMenu(Canvas quitMenu){
+		quitMenu.enabled = true;
+	}
+
+	public void HideQuitMenu(Canvas quitMenu){
+		quitMenu.enabled = false;
+	}
 }
