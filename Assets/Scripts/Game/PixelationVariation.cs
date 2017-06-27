ad416858e5657ce929951e9b9331abff7c4cee51
AndrewMoffat <moffatandr@myvuw.ac.nz>
2017-06-27 15:38:12 +1200

    Fixed Pxl scripts etc.

--- Assets/Scripts/Game/PixelationVariation.cs
+++ Assets/Scripts/Game/PixelationVariation.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PixelationVariation : MonoBehaviour {
-	public Renderer rendGame;
+	private Renderer rendGame;
+	private float time2Pxl;
+	private float pxlTime;
 	// Use this for initialization
 	void Start () {
 		rendGame = GetComponent<Renderer>();
-		rendGame.material.shader = Shader.Find("Pxl");
+		rendGame.material.shader = Shader.Find("PxlShader");
+		pxlTime = 0;
+		time2Pxl = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		this.rendGame.material.SetFloat("_Pxlation", 2);	
+		if (time2Pxl == 0) {
+			time2Pxl = Random.Range (70, 350);
+		}
+		if (time2Pxl > 1) {
+			time2Pxl--;
+			this.rendGame.material.SetFloat("_PxlSize", 7);	
+		}
+		if (time2Pxl == 1 && pxlTime == 0) {
+			pxlTime = Random.Range (20, 40);
+		}
+		if (time2Pxl == 1 && pxlTime > 0) {
+			pxlTime--;
+			this.rendGame.material.SetFloat("_PxlSize", 20);	
+			if(pxlTime==0){
+				time2Pxl=0;
+			}
+		}
+			
 	}
 }
