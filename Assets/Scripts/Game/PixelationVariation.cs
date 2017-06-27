b5d9de82b84155a9ef2c26962a724978badcbd2f
Guest User <Guest@g-01-06-lab.mddn.vuw.ac.nz>
2017-06-27 14:33:10 +1200

    Fixed lighting

---
+++ Assets/Scripts/Game/PixelationVariation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelationVariation : MonoBehaviour {
+	public Renderer rendGame;
+	// Use this for initialization
+	void Start () {
+		rendGame = GetComponent<Renderer>();
+		rendGame.material.shader = Shader.Find("Pxl");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.rendGame.material.SetFloat("_Pxlation", 2);	
+	}
+}
